7983a5cd899f2a446b4d59b69f801ddb287261a8
Jc Souza <jcsantos.souza1988@gmail.com>
2025-04-22 21:44:02 -0300

    Adicionando arquivos na aula07

---
+++ aulas/aula07/App.js
@@ -0,0 +1,15 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import Pilha from "./routes/Pilha";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Pilha />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
