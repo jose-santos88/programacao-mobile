dd6b369acb4a67785dcc87a0b88c18b3930794ad
Jc Souza <jcsantos.souza1988@gmail.com>
2025-05-22 21:41:14 -0300

    Adicionando arquivos da aula09

---
+++ aulas/aula09/components/ListaVertical.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { View, FlatList } from "react-native";
+import { List, Text } from "react-native-paper";
+
+import { LojaContext } from "../contexts/LojaContext";
+
+function ListaVertical() {
+  const { lojas } = useContext(LojaContext);
+
+  return (
+    <View>
+      <Text variant="titleMedium">Lojas</Text>
+      <FlatList
+        data={lojas}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          <List.Item title={item.nome} description={`* ${item.nota}`} />
+        )}
+      />
+    </View>
+  );
+}
+
+export default ListaVertical;
---
+++ aulas/aula09/screens/Home.js
@@ -0,0 +1,33 @@
+import { useContext, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+
+import { LojaContext } from "../contexts/LojaContext";
+import ListaVertical from "../components/ListaVertical";
+import ListaHorizontal from "../components/ListaHorizontal";
+
+function Home() {
+  const { carregando, carregarDados } = useContext(LojaContext);
+
+  useEffect(() => {
+    carregarDados();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      {carregando ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size={"large"} />
+        </View>
+      ) : (
+        <>
+          <ListaHorizontal />
+          <ListaVertical />
+        </>
+      )}
+    </View>
+  );
+}
+
+export default Home;
